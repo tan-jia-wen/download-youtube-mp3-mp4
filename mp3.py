c9a9e66a406d6f9502e87100f8889b4c050beb1c
tan-jia-wen <jw.jiawen02@gmail.com>
2025-09-02 23:36:43 +0800

    updated code
    changed save location of converted media

--- mp3.py
+++ mp3.py
@@ -1,13 +1,18 @@
+import os
 import yt_dlp
+
+os.makedirs("Downloaded", exist_ok=True)
 
 a = input("Enter URL: ")
 
 ydl_opts = {
     'format': 'bestaudio/best',
+    'outtmpl': 'Downloaded/%(title)s.%(ext)s',  # save inside Downloaded folder
     'postprocessors':[{
-    'key':'FFmpegExtractAudio',
-    'preferredcodec': 'mp3',
-    'preferredquality': '192',}],
+        'key':'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
 }
 
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
--- mp4.py
+++ mp4.py
@@ -1,9 +1,13 @@
+import os
 import yt_dlp
+
+os.makedirs("Downloaded", exist_ok=True)
 
 a = input("Enter URL: ")
 
 ydl_opts = {
     'format': 'best',
+    'outtmpl': 'Downloaded/%(title)s.%(ext)s',  # save inside Downloaded folder
     'postprocessors': [{
         'key': 'FFmpegVideoConvertor',
         'preferedformat': 'mp4',
